832789738d013b28ef551c7274af12a2914a3daf
Rabi A <41875211+Dgruploads@users.noreply.github.com>
2026-05-06 16:21:05 +0530

    Create route_table_pri.tf

---
+++ eks_setup_terraform/route_table_pri.tf
@@ -0,0 +1,19 @@
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+resource "aws_route" "private_internet" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private_assoc_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_assoc_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private_rt.id
+}

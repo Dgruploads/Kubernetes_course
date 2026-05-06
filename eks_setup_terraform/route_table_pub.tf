749bb69bf2e3356196b5fb42170607bf8a2af90f
Rabi A <41875211+Dgruploads@users.noreply.github.com>
2026-05-06 16:21:38 +0530

    Create route_table_pub.tf

---
+++ eks_setup_terraform/route_table_pub.tf
@@ -0,0 +1,14 @@
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_assoc_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
+}

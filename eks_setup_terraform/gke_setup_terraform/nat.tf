0f5ddbeaaf301ecba719f881a00afc2c182cb9e7
Rabi A <41875211+Dgruploads@users.noreply.github.com>
2026-05-07 19:33:24 +0530

    Create nat.tf

---
+++ eks_setup_terraform/gke_setup_terraform/nat.tf
@@ -0,0 +1,7 @@
+resource "google_compute_router_nat" "nat" {
+  name                               = "gke-nat"
+  router                             = google_compute_router.router.name
+  region                             = var.region_name
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
